80d509f18793c2abe1325921e0dcdcceea936e58
rektide de la faye <rektide@voodoowarez.com>
2018-03-06 21:56:15 -0500

    Start uniquify.
    5to6-codemod isn't de-clashing the case of module.exports having
    a property set to a top level declaration. This causes a clash. For
    example:
    `module.exports = { myDeclaration }`
    Would still let the current "myDeclaration" declaration exist, while
    also trying to write an ExportNamedDeclaration that will clash,
    re-defining the existing declaration. This new plugin proposes to
    re-write the file to detect this clash, & rename the existing
    declaration and all references to it.
    
    No implementation at this stage, just skeleton.

--- esm-bare-to-browser.js
+++ esm-bare-to-browser.js
@@ -5,6 +5,7 @@ import f2sNoStrict from "5to6-codemod/transforms/no-strict.js"
 import f2sUtil from "5to6-codemod/utils/main.js"
 import eb2bResolve from "./resolve.js"
 import eb2bPrefix from "./prefix.js"
+import eb2bUniquify from "./uniquify.js"
 
 export async function transform( file, api, options){
 	file.source= f2sExports( file, api, options)
@@ -13,6 +14,7 @@ export async function transform( file, api, options){
 	file.source= f2sNoStrict( file, api, options)
 	file.source= await eb2bResolve( file, api, options)
 	file.source= eb2bPrefix( file, api, options)
+	file.source= eb2bUniquify( file, api, options)
 	return file.source
 }
 export default transform
---
+++ uniquify.js
@@ -0,0 +1,16 @@
+/**
+  A CJS export does not clash with esm export scope. This is a major incompatibility hurdle that we're going to do unhygenic ridiculous schenanigans to crudely hack through.
+  Find all exports, then mangle all declarations that are clashes, then re-write any references to 
+  This could/should probably, ideally, be written into 5to6-codemod itself.
+*/
+export default function( file, api, options){
+	options= options|| {}
+	var
+	  j= api.jscodeshift,
+	  root= j( file.source),
+	  // read all export values
+	  // rename any declarations using an export name
+	  // rename any references to the export
+	  renamed2= root
+	return renamed2.toSource()
+}
