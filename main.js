3e4552ecaf56834c67379132a5ad5d90b2ceaba6
rektide de la faye <rektide@voodoowarez.com>
2018-03-10 17:37:59 -0500

    Default path generation was broken, reorder to fix.

--- main.js
+++ main.js
@@ -5,20 +5,20 @@ process.on("unhandledRejection", console.error)
 process.on("uncaughtException", console.error)
 
 export async function main(){
-	var
-	  stripped= import.meta.url.slice( 7),
-	  transformPath= dirname( stripped)+ sep+ "esm-bare-to-browser.js"
-	// the jscodeshift bin is unflexible. this is the low cost hack of it.
-	process.argv.push("-t", transformPath, "--no-babel")
-
-	// no location specified
+	// if no path is specified
 	if( process.argv.length<= 2){
-		// find non-dev packages
+		// default to all non-dev packages
 		var pkgs= new PackageDepends({ trim: true, modulesDirs: ["browser_modules", "node_modules"]})
 		for await( var pkg of pkgs.depends()){
 			process.argv.push( pkg)
 		}
 	}
+
+	var
+	  stripped= import.meta.url.slice( 7),
+	  transformPath= dirname( stripped)+ sep+ "esm-bare-to-browser.js"
+	// the jscodeshift bin is unflexible. this is the low cost hack of it.
+	process.argv.push("-t", transformPath, "--no-babel")
 
 	// import() because it will immediately invoke
 	// if this were statically imported just requiring main would run
